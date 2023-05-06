0032ebeeb498a7de0e5c2e542d507ec165050e5b
TZeteny <99351536+TZeteny@users.noreply.github.com>
2023-05-06 14:48:32 +0200

    Add files via upload

---
+++ Beadandó/include/model.h
@@ -0,0 +1,64 @@
+#ifndef OBJ_MODEL_H
+#define OBJ_MODEL_H
+
+#define TRUE 1
+#define FALSE 0
+
+#define INVALID_VERTEX_INDEX 0
+
+//3D vertex
+typedef struct Vertex {
+    double x;
+    double y;
+    double z;
+} Vertex;
+
+//2D texture vetex
+typedef struct TextureVertex {
+    double u;
+    double v;
+} TextureVertex;
+
+//Point of the face
+typedef struct FacePoint {
+    int vertex_index;
+    int texture_index;
+    int normal_index;
+} FacePoint;
+
+//Triangle as facepoint triplet
+typedef struct Triangle {
+    struct FacePoint points[3];
+} Triangle;
+
+//3D model with texture
+typedef struct Model {
+    int n_vertices;
+    int n_texture_vertices;
+    int n_normals;
+    int n_triangles;
+    Vertex* vertices;
+    TextureVertex* texture_vertices;
+    Vertex* normals;
+    Triangle* triangles;
+} Model;
+
+//Types of the considered elements
+typedef enum {
+    NONE,
+    VERTEX,
+    TEXTURE_VERTEX,
+    NORMAL,
+    FACE
+} ElementType;
+
+//Initialize the model structure
+void init_model(Model* model);
+
+//Allocate memory for model
+void allocate_model(Model* model);
+
+//Release the allocated memory of the model
+void free_model(Model* model);
+
+#endif
---
+++ Beadandó/include/scene.h
@@ -0,0 +1,44 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include "camera.h"
+#include "texture.h"
+#include "utils.h"
+#include "model.h"
+
+#include <obj/model.h>
+
+typedef struct Scene {
+    //check if necessary size
+    Model model[15];
+
+    Material material;
+    Material sky_material;
+    Material grass_material;
+
+    //check if necessary size
+    GLuint textures[18];
+
+    float rotation;
+} Scene;
+
+//use the txt files to load in the correct models and textures
+void object_reader(Scene* scene);
+void texture_reader(Scene* scene);
+
+//Initialize the scene by loading models
+void init_scene(Scene* scene);
+
+//Set the lighting of the scene
+void set_lighting();
+
+//Set the current material
+void set_material(const Material* material);
+
+//Draw and update the scene objects
+void update_scene(Scene* scene, double time);
+
+//Draw the scene objects
+void draw_scene(const Scene* scene);
+
+#endif
